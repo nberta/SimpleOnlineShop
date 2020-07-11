b2bc6ac867b8e016482a7176b12b52a3038054f6
MargulanAubakirov <nalugram@gmail.com>
2020-07-11 11:02:22 -0500

    Product Controller - CRUD operations Added CategoryService with List<category> change in ProductService here must be List<Products> for getAllConfirmed

--- src/main/java/edu/miu/simpleshop/service/impl/ProductServiceImpl.java
+++ src/main/java/edu/miu/simpleshop/service/impl/ProductServiceImpl.java
@@ -1,11 +1,16 @@
 package edu.miu.simpleshop.service.impl;
 
 import edu.miu.simpleshop.domain.Product;
+import edu.miu.simpleshop.repository.CategoryRepository;
+import edu.miu.simpleshop.repository.ProductRepository;
 import edu.miu.simpleshop.service.ProductService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,28 +18,38 @@ import java.util.List;
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+
     @Override
     public Product getById(Long id) {
-        return null;
+        return productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Product save(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
     public Collection<Product> save(Collection<Product> products) {
-        return null;
+        return productRepository.saveAll(products);
     }
 
     @Override
     public Product delete(Long id) {
+//        Product product = productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         return null;
     }
 
     @Override
     public List<Product> getAllUnconfirmedProducts() {
+        List<Product> result;
         return null;
     }
 
@@ -45,6 +60,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Collection<Product> getByCategoryId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(Long id) {
         return null;
     }
 }
