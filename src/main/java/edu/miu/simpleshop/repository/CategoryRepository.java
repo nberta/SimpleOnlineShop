43279997b41955a0312b9fcfe90ac45220b8945f
Nis <36338262+bouncerate@users.noreply.github.com>
2020-07-11 04:21:25 -0500

    - Domains Updated, UML added.

--- src/main/java/edu/miu/simpleshop/repository/CategoryRepository.java
+++ src/main/java/edu/miu/simpleshop/repository/CategoryRepository.java
@@ -1,10 +1,9 @@
 package edu.miu.simpleshop.repository;
 
-import edu.miu.simpleshop.domain.Buyer;
+import edu.miu.simpleshop.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BuyerRepository extends JpaRepository<Buyer, Long> {
-
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- src/main/java/edu/miu/simpleshop/repository/OrderRepository.java
+++ src/main/java/edu/miu/simpleshop/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 
 package edu.miu.simpleshop.repository;
 
+import edu.miu.simpleshop.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderRepository{
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 
 }
