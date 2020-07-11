48f04600ff97450a5e7d5b15ec83c305abd3112e
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-07-11 12:04:23 -0500

    update FollowController

--- src/main/java/edu/miu/simpleshop/controller/FollowController.java
+++ src/main/java/edu/miu/simpleshop/controller/FollowController.java
@@ -1,10 +1,7 @@
 package edu.miu.simpleshop.controller;
 
 import edu.miu.simpleshop.domain.*;
-import edu.miu.simpleshop.service.BuyerService;
-import edu.miu.simpleshop.service.FollowService;
-import edu.miu.simpleshop.service.ProductService;
-import edu.miu.simpleshop.service.SellerService;
+import edu.miu.simpleshop.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,24 +10,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.security.Principal;
 import java.util.List;
 
-
-public class FollowController {
-
     @Controller
     @RequestMapping("/follow")
-    public class FollowerController {
-        private FollowService followService;
-        private SellerService sellerService;
-        private BuyerService buyerService;
-        private ProductService productService;
+    public class FollowController {
 
         @Autowired
-        public FollowerController(ProductService productService,FollowService followService, SellerService sellerService, BuyerService buyerService) {
-            this.followService = followService;
-            this.sellerService = sellerService;
-            this.buyerService = buyerService;
-            this.productService=productService;
-        }
+        private FollowService followService;
+
 
         @GetMapping("/getSeller")
         public String getSeller(@PathVariable Long id,  Model model){
@@ -40,25 +26,20 @@ public class FollowController {
 
 
         @GetMapping("/followSave")
-        public String saveFollower(Model model, @RequestParam("sellerId") Long sellerId, Principal principal, RedirectAttributes redirectAttributes){
-            String email = principal.getName();
-            Buyer buyer = buyerService.getByEmail(email);
-            Seller seller=sellerService.getById(sellerId);
+        public String saveFollower(Model model, @RequestParam("sellerId") Long buyerId, RedirectAttributes redirectAttributes){
+            Buyer buyer = buyerService.getById(buyerId);
             Follow follow= new Follow();
             follow.setBuyer(buyer);
-            follow.setSeller(seller);
             followService.save(follow);
             redirectAttributes.addFlashAttribute("successMessage","You are successfuly following this seller "+ seller.getFullName());
             return "redirect:/follower/getSeller";
-
         }
 
-        @GetMapping("/getFollowed")
-        public String getFollowed(Model model,Principal principal){
+       @GetMapping("/getFollowed")
+        public String getFollowed(Model model, Long buyerId){
             List<Seller> sellerList;
-            String email = principal.getName();
-            Buyer buyer = buyerService.findByEmail(email);
-            sellerList=followService.findAllByProductAndReviewStatus(Follower.FollowerStatus.follow,buyer.getId());
+            Buyer buyer = buyerService.getById(buyerId);
+            sellerList=followService.getFollowByBuyerAndSeller(Follow.FollowStatus.follow,buyer.getId());
             //sellerList.forEach(x->System.out.println(x));
             model.addAttribute("sellerList",sellerList);
             return "buyer/followedListSeller";
@@ -67,13 +48,13 @@ public class FollowController {
 
         //UNFOLLOW
         @GetMapping("/UnfollowSave")
-        public String UnfollowSave(Model model, @RequestParam("sellerId") Long sellerId, Principal principal, RedirectAttributes redirectAttributes){
+        public String UnfollowSave(Model model, @RequestParam("sellerId") Long sellerId, Long buyerId, Principal principal, RedirectAttributes redirectAttributes){
             String email = principal.getName();
             Buyer buyer = buyerService.findByEmail(email);//email will help us to validate the buyer later
             Seller seller=sellerService.getById(sellerId);
-            Follow follow=FollowService.findFollowByBuyerAndSeller(sellerId,buyerId);
+            Follow follow=followService.getFollowByBuyerAndSeller(sellerId,buyerId);
             follow.setFollowStatus(Follow.FollowStatus.unfollow);//i need to have followStatus
-            FollowService.delete(follow);
+            followService.delete(follow);
             redirectAttributes.addFlashAttribute("successMessage","You are successful Unfollowing this seller "+ seller.getFullName());
             return "redirect:/follow/getFollowed";
 
