b52cd5888efae5b711327b3e88237772149cfe96
Hworkneh <48295789+hworkneh@users.noreply.github.com>
2020-07-11 10:18:49 -0500

    update FollowController

--- src/main/java/edu/miu/simpleshop/service/FollowService.java
+++ src/main/java/edu/miu/simpleshop/service/FollowService.java
@@ -12,11 +12,9 @@ import java.util.List;
 public interface FollowService {
     public List<Follow> findAll();
     public Follow save(Follow follow);
-    public Follow find(Long id);
-    public Follow put(Follow follow);
     public void delete(Follow follower);
-    public List<Seller> findAllBySeller(Long Id);
+    public List<Seller> getAllBySeller(Long Id);
   //  public List<Seller> findAllByProductAndReviewStatus(Follow.FollowStatus followerStatus, Long id);
-    public Follow findFollowByBuyerAndSeller(Long sellerId, Long buyerId);
+    public Follow getFollowByBuyerAndSeller(Long sellerId, Long buyerId);
 
 }
