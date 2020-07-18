e78d1bd112d3b5a361cdf9e55a56457f86b6084f
bouncerate <ndnp1118@gmail.com>
2020-07-18 10:38:38 -0500

    -bunch of stuff. Update posted on teams

---
+++ src/main/java/edu/miu/simpleshop/api/MainApi.java
@@ -0,0 +1,31 @@
+package edu.miu.simpleshop.api;
+
+import edu.miu.simpleshop.domain.Product;
+import edu.miu.simpleshop.service.ProductService;
+import javassist.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class MainApi {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/getAll")
+    @ResponseBody
+    public List<Product> getAllAdminsByStatus() {
+        return service.getAllUnconfirmedProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id)throws NotFoundException{
+        return service.getById(id);
+    }
+
+
+}
